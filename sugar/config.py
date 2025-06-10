f49f730f0dd418ff6ab1f75b694a525860ae2f79
stepollo2 <stepolloz@protonmail.com>
2025-06-10 10:34:38 +0200

    add metal

--- sugar/config.py
+++ sugar/config.py
@@ -4,7 +4,7 @@
 __all__ = ['base_default_settings', 'GetEnv', 'ChainSettings', 'validate_settings', 'get_env', 'make_settings',
            'make_op_chain_settings', 'make_base_chain_settings', 'make_uni_chain_settings',
            'make_soneium_chain_settings', 'make_fraxtal_chain_settings', 'make_ink_chain_settings',
-           'make_lisk_chain_settings']
+           'make_lisk_chain_settings', 'make_metal_chain_settings']
 
 # %% ../src/config.ipynb 3
 import os
@@ -250,7 +250,7 @@ def make_lisk_chain_settings(get_env: GetEnv = get_env, **kwargs) -> ChainSettin
         "wrapped_native_token_addr": "0x4200000000000000000000000000000000000006",#
         "sugar_contract_addr": "0x1443c0757f77c04bd514427d2bE356A5834E4167",#
         "sugar_rewards_contract_addr": "0xB1d0DFFe6260982164B53EdAcD3ccd58B081889d",#
-        "slipstream_contract_addr": "0x222ed297aF0560030136AE652d39fa40E1B72818",
+        "slipstream_contract_addr": "0xB98fB4C9C99dE155cCbF5A14af0dBBAd96033D6f",
         "nfpm_contract_addr": "0x991d5546c4b442b4c5fdc4c8b8b8d131deb24702",#
         "price_oracle_contract_addr": "0x024503003fFE9AF285f47c1DaAaA497D9f1166D0",#
         "router_contract_addr": "0x3a63171dd9bebf4d07bc782fecc7eb0b890c2a45",#
@@ -262,3 +262,24 @@ def make_lisk_chain_settings(get_env: GetEnv = get_env, **kwargs) -> ChainSettin
         "excluded_tokens_addrs": "0xE3Ae7873B76722Ba23b1EEEEE544a75eDe891a66"#random one
     }
     return make_settings("1135", "Lisk", chain_settings=d, get_env=get_env, **kwargs)
+
+# %% ../src/config.ipynb 20
+def make_metal_chain_settings(get_env: GetEnv = get_env, **kwargs) -> ChainSettings:
+    d = {
+        "rpc_uri": "https://rpc.metall2.com",
+        "pools_count_upper_bound": 20,
+        "wrapped_native_token_addr": "0x4200000000000000000000000000000000000006",#
+        "sugar_contract_addr": "0x471C34138D06001AEe399af9EBEbbdF680b966B3",#
+        "sugar_rewards_contract_addr": "0xbDD1d5A9d9566F575bC59cE33C8F77ACa5cF924b",#
+        "slipstream_contract_addr": "0x222ed297aF0560030136AE652d39fa40E1B72818",#
+        "nfpm_contract_addr": "0x991d5546c4b442b4c5fdc4c8b8b8d131deb24702",#
+        "price_oracle_contract_addr": "0x3e71CCdf495d9628D3655A600Bcad3afF2ddea98",#
+        "router_contract_addr": "0x3a63171dd9bebf4d07bc782fecc7eb0b890c2a45",#
+        "quoter_contract_addr": "0x2f7150B288ef1cc553207bD9fbd40D4e0e093B24",#
+        "swapper_contract_addr": "0x652e53C6a4FE39B6B30426d9c96376a105C89A95",#
+        "token_addr": "0x7f9AdFbd38b669F03d1d11000Bc76b9AaEA28A81",
+        "stable_token_addr": "0x51E85d70944256710cb141847F1a04f568C1Db0e",
+        "connector_tokens_addrs": "0x4200000000000000000000000000000000000006,0x7f9AdFbd38b669F03d1d11000Bc76b9AaEA28A81",
+        "excluded_tokens_addrs": "0xE3Ae7873B76722Ba23b1EEEEE544a75eDe891a66"#random one
+    }
+    return make_settings("1750", "Metal", chain_settings=d, get_env=get_env, **kwargs)
